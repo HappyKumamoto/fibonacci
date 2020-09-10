bb768ef7eb54dc5e796923b3d5b9cccbb4cdaaaf
HappyKumamoto <zen.word+hub@gmail.com>
2020-09-11 02:10:42 +0900

    メモ化によるアルゴリズムの改善

--- app1.js
+++ app1.js
@@ -1,11 +1,14 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
-  }
-  return fib(n-1)+fib(n-2);
+  if (memo.has(n)) {//答えを知っていたらそれを返す
+    return memo.get(n)
+  } 
+const value = fib(n-1)+ fib(n-2);//知らなかったら計算する
+memo.set(n, value);              //計算結果も記録する
+return value;                    //その値を返す
 }
 const length = 40;
 for (let i = 0; i<= length; i++) {
